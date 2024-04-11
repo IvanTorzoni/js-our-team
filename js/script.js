eec622987a74ece7107b9052f97d8eae98e814c6
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-11 15:26:03 +0200

    feat: added console log for the visualization of every elements in the objects

--- js/script.js
+++ js/script.js
@@ -5,42 +5,42 @@
 // Creo l'array con gli object
 const team = [
     // All'interno dell' array inseriamo tutti gli object. Negli object inseriamo le caratteristiche delle singole persone
-    
+
     //Team member 1
     {
         name: "Wayne Barnett",
         role: "Founder & CEO",
         image: "wayne-barnett-founder-ceo.jpg"
     },
-    
+
     //Team member 2
     {
         name: "Angela Caroll",
         role: "Chief Editor",
         image: "angela-caroll-chief-editor.jpg"
     },
-    
+
     //Team member 3 
     {
         name: "Walter Gordon",
         role: "Office Manager",
         image: "walter-gordon-office-manager.jpg"
     },
-    
+
     //Team member 4
     {
         name: "Angela Lopez",
         role: "Social Media Manager",
         image: "angela-lopez-social-media-manager.jpg"
     },
-    
+
     //Team member 5
     {
         name: "Scott Estrada",
         role: "Developer",
         image: "scott-estrada-developer.jpg"
     },
-    
+
     //Team member 6
     {
         name: "Barbara Ramos",
@@ -48,4 +48,15 @@ const team = [
         image: "barbara-ramos-graphic-designer.jpg"
     },
 ];
+
+// Faccio passare l'array e stampo in console tutti gli object all'interno
+for (i = 0; i < team.length; i++) {
+    const teamInfos = team[i];
+    console.log(teamInfos);
+    //Milestone 1  
+    // Stampo i singoli elementi degli object nella console
+    for (let key in teamInfos) {
+        console.log(teamInfos[key]);
+    }
+}
 

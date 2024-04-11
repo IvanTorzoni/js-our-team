6c755b7cbe6626ecefe96771f70d2baf01791f32
IvanTorzoni <torzoni.ivan00@gmail.com>
2024-04-11 15:22:15 +0200

    feat: creation of the array of object for team members

--- js/script.js
+++ js/script.js
@@ -1,2 +1,51 @@
-// TEST
-alert("ciao");
+// Utilizzando i dati forniti, creare un array di oggetti per rappresentare i membri del team.
+// Ogni membro è caratterizzato dalle seguenti informazioni: nome, ruolo e foto.
+
+// Milestone 0 
+// Creo l'array con gli object
+const team = [
+    // All'interno dell' array inseriamo tutti gli object. Negli object inseriamo le caratteristiche delle singole persone
+    
+    //Team member 1
+    {
+        name: "Wayne Barnett",
+        role: "Founder & CEO",
+        image: "wayne-barnett-founder-ceo.jpg"
+    },
+    
+    //Team member 2
+    {
+        name: "Angela Caroll",
+        role: "Chief Editor",
+        image: "angela-caroll-chief-editor.jpg"
+    },
+    
+    //Team member 3 
+    {
+        name: "Walter Gordon",
+        role: "Office Manager",
+        image: "walter-gordon-office-manager.jpg"
+    },
+    
+    //Team member 4
+    {
+        name: "Angela Lopez",
+        role: "Social Media Manager",
+        image: "angela-lopez-social-media-manager.jpg"
+    },
+    
+    //Team member 5
+    {
+        name: "Scott Estrada",
+        role: "Developer",
+        image: "scott-estrada-developer.jpg"
+    },
+    
+    //Team member 6
+    {
+        name: "Barbara Ramos",
+        role: "Graphic Designer",
+        image: "barbara-ramos-graphic-designer.jpg"
+    },
+];
+
